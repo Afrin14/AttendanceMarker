b0dd5e49f694562289a18ed5372d95ce7d663dfe
Afrin14 <afrin.nz13@gmail.com>
2019-09-05 06:59:34 +0530

    Remover of Unnecessary codes

--- app/src/main/java/com/github/afrin14/attendancemarker/SettingsActivity.java
+++ app/src/main/java/com/github/afrin14/attendancemarker/SettingsActivity.java
@@ -36,36 +36,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         addPreferencesFromResource(R.xml.pref_general);
 
 
-    /*    Preference myPref = findPreference("clear_schedule");
-        myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
-                final AlertDialog.Builder alert = new AlertDialog.Builder(base);
-                alert.setTitle("Are you sure ?");
-                alert.setMessage("This will wipe your entire scheduling data");
-                alert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String qu = "DROP TABLE SCHEDULE";
-                        if (AppBase.handler.execAction(qu)) {
-                            Toast.makeText(getBaseContext(), "Done", Toast.LENGTH_LONG).show();
-                            AppBase.handler.createTable();
-                            dialog.dismiss();
-                        }
-                    }
-                });
-                alert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        return;
-                    }
-                });
-                alert.show();
-                return true;
-            }
-        });
-
-        */
         Preference myPref = findPreference("clear_attendance");
         myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference preference) {
@@ -96,36 +66,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             }
         });
 
-      /*  myPref = findPreference("clear_notes");
-        myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
-                final AlertDialog.Builder alert = new AlertDialog.Builder(base);
-                alert.setTitle("Are you sure ?");
-                alert.setMessage("This will wipe your entire Notes data");
-                alert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String qu = "DROP TABLE NOTES";
-                        if(AppBase.handler.execAction(qu))
-                        {
-                            Toast.makeText(getBaseContext(),"Done",Toast.LENGTH_LONG).show();
-                            AppBase.handler.createTable();
-                            dialog.dismiss();
-                        }
-                    }
-                });
-                alert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        return;
-                    }
-                });
-                alert.show();
-                return true;
-            }
-        });
-    */
         myPref = findPreference("clear_student");
         myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference preference) {
